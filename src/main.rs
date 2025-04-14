d80ee837b86520c9bc999110f38dfd5045a65d57
Guilherme <guipuszkarek@gmail.com>
2025-04-14 10:29:30 -0300

    improve code quality

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod cmus;
 mod lyrics;
 mod models;
 
-fn main() -> Result<()> {
+fn main() -> () {
     // Track current song path
     let current_song_path = Arc::new(Mutex::new(String::new()));
     let lyrics = Arc::new(Mutex::new(Result::<Option<Vec<LyricLine>>>::Ok(None)));
@@ -16,27 +16,34 @@ fn main() -> Result<()> {
     // Main loop
     loop {
         // Get current song info from cmus
-        let song_info = cmus::get_current_song()?;
-        if song_info.is_none() {
-            cmus::render_no_song();
-            continue;
-        }
+        let song_info = cmus::get_current_song();
 
-        let unwrapped_song_info = song_info.unwrap();
-        if unwrapped_song_info.file_path != current_song_path.lock().unwrap().as_str() {
-            // Song changed
-            current_song_path.lock().unwrap().clear();
-            current_song_path
-                .lock()
-                .unwrap()
-                .push_str(&unwrapped_song_info.file_path);
-            // Update lyrics
-            let new_lyrics = lyrics::get_lyrics(&unwrapped_song_info);
-            *lyrics.lock().unwrap() = new_lyrics;
-        }
+        match song_info {
+            Ok(Some(song)) => {
+                if song.file_path != current_song_path.lock().unwrap().as_str() {
+                    // Song changed
+                    current_song_path.lock().unwrap().clear();
+                    current_song_path.lock().unwrap().push_str(&song.file_path);
+                    // Update lyrics
+                    let new_lyrics = lyrics::get_lyrics(&song);
+                    *lyrics.lock().unwrap() = new_lyrics;
+                }
 
-        if let Err(e) = update_lyrics(&lyrics.lock().unwrap(), &unwrapped_song_info) {
-            eprintln!("Error updating lyrics: {}", e);
+                if let Err(e) = update_lyrics(&lyrics.lock().unwrap(), &song) {
+                    eprintln!("Error updating lyrics: {}", e);
+                }
+            }
+            Ok(None) => {
+                // No song playing
+                cmus::render_no_song();
+                thread::sleep(Duration::from_secs(1));
+                continue;
+            }
+            Err(e) => {
+                eprintln!("Error getting current song info: {}", e);
+                cmus::render_no_song();
+                thread::sleep(Duration::from_secs(2));
+            }
         }
     }
 }
